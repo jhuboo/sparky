b35ff741a77b75fd0c8c0c68dc94a095adb4b046
Anvesh <rgjhuboo@gmail.com>
2022-03-17 16:33:20 -0400

    Add autostart.sh so that Sparky software starts automatically

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash /home/pi/sparky/autostart.sh
+
+# autostart.sh
+# NOTE:
+# Part of this file needs to uncommented so that your own Sparky can run
+# automatically every time it is booted.
+
+# 1. Edit /etc/rc.local as root
+# 2. Add the line 'python3 /home/pi/myscript.py' or
+# 		  '/home/pi/myscript.sh'
+#    depending on what you want to autostart
+# 3. Make sure that rc.local is executable -> $ sudo chmod +x /etc/rc.local
+# 4. Reboot your Raspberry Pi to test
+
+# To be updated
